6c9df6d88fda64a9e4c14ff941a5ea7742986b9b
plelle <philip.lelle@gmail.com>
2017-01-24 20:53:43 -0500

    initial setup

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE wornbyjake;
+USE wornbyjake;
+
+CREATE TABLE user {
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(100) NOT NULL,
+    username varchar(100) NOT NULL,
+    email varchar(254) NOT NULL,
+    password varchar(100) NOT NULL,
+    company_name varchar(100),
+    logo LONGBLOB,
+    phone_number int(10) NOT NULL,
+    profile_desc varchar(255),
+    PRIMARY KEY (id)
+},
+
+CREATE TABLE product {
+    id int NOT NULL AUTO_INCREMENT,
+    prod_name varchar(100) NOT NULL,
+    prod_desc varchar(255) NOT NULL,
+    prod_cat varchar(100) NOT NULL,
+    prod_price decimal(10,2) NOT NULL,
+    prod_pic1 LONGBLOB NOT NULL,
+    prod_pic2 LONGBLOB,
+    prod_pic3 LONGBLOB,
+    prod_pic4 LONGBLOB,
+    prod_pic5 LONGBLOB,
+    prod_pic6 LONGBLOB,
+    prod_trade varchar(100) NOT NULL
+}
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (name, username, email, password, company_name, logo, phone_number, profile_desc) VALUES ("Jake Tuma", "jake", "jake@wornbyjake.com", "jake122", "Worn By Jake", "", "386-681-8060", "Owner bitch");
+INSERT INTO user (name, username, email, password, company_name, logo, phone_number, profile_desc) VALUES ("Jeff Bauer", "jeff", "jeff@wornbyjake.com", "jeff1234", "Worn By Jake", "", "407-690-1019", "2nd Owner bitch");
+INSERT INTO user (name, username, email, password, company_name, logo, phone_number, profile_desc) VALUES ("Stefan Beaulieu", "stefan", "stefan@wornbyjake.com", "stefan1234", "Worn By Jake", "", "407-562-7718", "3rd Owner bitch");
+INSERT INTO user (name, username, email, password, company_name, logo, phone_number, profile_desc) VALUES ("Phil Lelle", "phil", "phil@wornbyjake.com", "phil1234", "Worn By Jake", "", "321-231-8766", "4th Owner bitch");
+
