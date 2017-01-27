0b8b47a4f958a0fd3206785571a7d6f5c6a22bfc
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2017-01-27 18:48:09 -0500

    #13 - added vaildation to modal files

--- models/product.js
+++ models/product.js
@@ -4,28 +4,38 @@ module.exports = function(sequelize, DataTypes) {
 
 prod_name: {
     type: Sequelize.STRING,
+    validate: {
     allowNull: false
+    },
 },
 prod_desc: {
     type: Sequelize.STRING,
+    validate: {
     allowNull: false,
-    len: [25, 200],
+    len: [25, 200]
+    },
 },
 prod_cat: {
     type: Sequelize.STRING,
+    validate: {
     allowNull: false,
     len: [5, 50]
+    },
 },
 prod_price: {
     type: Sequelize.INTEGER,
+    validate: {
     allowNull: false,
     isDecimal: true,
     isNumeric: true
+    },
 },
 prod_trade: {
     type: Sequelize.BOOLEAN,
+    validate: {
     isAlpha: true,
     allowNull: false
+    }
 }
   });
 };
--- models/user.js
+++ models/user.js
@@ -3,37 +3,51 @@ module.exports = function(sequelize, DataTypes) {
     // Giving the Author model a name of type STRING
   name: { 
       type: Sequelize.STRING,
+      validate: {
       allowNull: false,
       isAlpha: true
+      },
   },
   username: {
       type: Sequelize.STRING,
+      validate: {
       allowNull: false,
       len: [3, 12]
+      },
   },
   email: {
       type: Sequelize.STRING,
+      validate: {
       allowNull: false,
       isEmail: true
+      },
   },
   password: {
       type: Sequelize.STRING,
+      validate: {
       allowNull: false,
       len: [6, 15]
+      },
   },
   company_name: {
       type: Sequelize.STRING,
+      validate: {
       allowNull: true
+      },
   },
   phone_number: {
       type: Sequelize.INTEGER,
+      validate: {
       allowNull: true,
       isNumeric: true
+      },
   },
   profile_desc: {
       type: Sequelize.STRING,
+      validate: {
       allowNull: true,
       len: [0, 200]
+      },
   }
    
 });
