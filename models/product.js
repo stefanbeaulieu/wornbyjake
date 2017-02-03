f407853b337eb9b8aa48a66582e620338fedcd46
Stefan Beaulieu <beaulieustefan@gmail.com>
2017-02-02 21:13:27 -0500

    #12 - Adding Product with NO Boolean

--- models/product.js
+++ models/product.js
@@ -1,42 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
-  var Product = sequelize.define("Product", {
-    // Giving the Author model a name of type STRING
+    var Product = sequelize.define("Product", {
+        // Giving the Author model a name of type STRING
 
-prod_name: {
-    type: DataTypes.STRING,
-    validate: {
-    allowNull: false
-    },
-},
-prod_desc: {
-    type: DataTypes.STRING,
-    validate: {
-    allowNull: false,
-    len: [25, 200]
-    },
-},
-prod_cat: {
-    type: DataTypes.STRING,
-    validate: {
-    allowNull: false,
-    len: [5, 50]
-    },
-},
-prod_price: {
-    type: DataTypes.INTEGER,
-    validate: {
-    allowNull: false,
-    isDecimal: true,
-    isNumeric: true
-    },
-},
-prod_trade: {
-    type: DataTypes.BOOLEAN,
-    validate: {
-    isAlpha: true,
-    allowNull: false
-    }
-}
-  });
-  return Product;
+        prod_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        prod_desc: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            len: [1, 200]
+        },
+        prod_cat: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            len: [1, 50]
+
+        },
+        prod_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            isDecimal: true,
+            isNumeric: true
+        },
+        prod_trade: {
+            type: DataTypes.BOOLEAN,
+            isAlpha: true,
+            allowNull: true
+
+        }
+    });
+    return Product;
 };
